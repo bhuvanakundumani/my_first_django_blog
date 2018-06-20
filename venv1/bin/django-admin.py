31e4cb46e8ffc6ed8559a965e77ca87f865fe31f
bhuvana <bhuvana.kundumani@gmail.com>
2018-06-20 11:39:08 +0530

    my first django girls app, first commit

---
+++ venv1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bhuvana/Library/Mobile Documents/com~apple~CloudDocs/Udemypy/djangogirls/venv1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
